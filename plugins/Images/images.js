c15b1ae82b12ef1b3bd512d1bb11b3cbe26b593c
Xeno <darkshot117@gmail.com>
2016-09-18 01:32:41 -0700

    Fixed searches

--- plugins/Images/images.js
+++ plugins/Images/images.js
@@ -2,22 +2,28 @@ var request = require("request");
 var AuthDetails = require("../../auth.json");
 
 exports.commands = [
-	//"image", //gives top image from google search
-	//"rimage", //gives random image from google search
-	//"ggif" //gives random gif from google search
+	"image", //gives top image from google search
+	"rimage", //gives random image from google search
+	"ggif", //gives random gif from google search
+	"imgur", //gives top image from google search on imgur.com
+	"google" //gives top image from google search on imgur.com
 ];
 
 exports.image = {
 	usage: "<search query>",
 	description: "gets the top matching image from google",
 	process: function(bot, msg, args) {
-		if(!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.imgur_google_custom_search){
+		if(!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.google_custom_search){
 			msg.channel.sendMessage("Image search requires both a YouTube API key and a Google Custom Search key!");
 			return;
 		}
-		//gets us a random result in first 5 pages
-		var page = 1; //we request 10 items
-		request("https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key + "&cx=" + AuthDetails.google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start="+page, function(err, res, body) {
+
+		var imageQueryCleaned = (args.replace(/\s/g, '+'));
+		var searchURL = "https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key
+				+ "&cx=" + AuthDetails.google_custom_search + "&searchType=image"
+				+ "&safe=medium&alt=json&q=" + imageQueryCleaned;
+
+		request(searchURL, function(err, res, body) {
 			var data, error;
 			try {
 				data = JSON.parse(body);
@@ -35,8 +41,8 @@ exports.image = {
 				msg.channel.sendMessage("No result for '" + args + "'");
 				return;
 			}
-			var randResult = data.items[0];
-			msg.channel.sendMessage(randResult.title + '\n' + randResult.link);
+			var result = data.items[0];
+			msg.channel.sendMessage(result.title + '\n' + result.link);
 		});
 	}
 }
@@ -45,13 +51,18 @@ exports.rimage = {
 	usage: "<search query>",
 	description: "gets a random image matching tags from google",
 	process: function(bot, msg, args) {
-		if(!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.imgur_google_custom_search){
+		if(!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.google_custom_search){
 			msg.channel.sendMessage("Image search requires both a YouTube API key and a Google Custom Search key!");
 			return;
 		}
 		//gets us a random result in first 5 pages
 		var page = 1 + Math.floor(Math.random() * 5) * 10; //we request 10 items
-		request("https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key + "&cx=" + AuthDetails.google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start="+page, function(err, res, body) {
+		var imageQueryCleaned = (args.replace(/\s/g, '+'));
+		var searchURL = "https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key
+				+ "&cx=" + AuthDetails.google_custom_search + "&searchType=image"
+				+ "&safe=medium&alt=json&num=10&start="+page +"&q=" + imageQueryCleaned;
+
+		request(searchURL, function(err, res, body) {
 			var data, error;
 			try {
 				data = JSON.parse(body);
@@ -81,7 +92,12 @@ exports.ggif = {
 	process : function(bot, msg, args) {
 		//gets us a random result in first 5 pages
 		var page = 1 + Math.floor(Math.random() * 5) * 10; //we request 10 items
-		request("https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key + "&cx=" + AuthDetails.imgur_google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start="+page+"&fileType=gif", function(err, res, body) {
+		var imageQueryCleaned = (args.replace(/\s/g, '+'));
+		var searchURL = "https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key
+				+ "&cx=" + AuthDetails.google_custom_search + "&searchType=image"
+				+ "&alt=json&num=10&start=" + page + "&fileType=gif&q=" + imageQueryCleaned;
+
+		request(searchURL, function(err, res, body) {
 			var data, error;
 			try {
 				data = JSON.parse(body);
@@ -103,5 +119,81 @@ exports.ggif = {
 			msg.channel.sendMessage(randResult.title + '\n' + randResult.link);
 		});
 
+	}
+}
+
+exports.imgur = {
+	usage: "<search query>",
+	description: "gets the top matching image from google on imgur.com",
+	process: function(bot, msg, args) {
+		if(!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.google_custom_search){
+			msg.channel.sendMessage("Image search requires both a YouTube API key and a Google Custom Search key!");
+			return;
+		}
+
+		var imageQueryCleaned = "site:imgur.com " + (args.replace(/\s/g, '+'));
+		var searchURL = "https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key
+				+ "&cx=" + AuthDetails.google_custom_search + "&searchType=image"
+				+ "&safe=medium&alt=json&q=" + imageQueryCleaned;// + "&as_sitesearch=imgur.com";
+
+		request(searchURL, function(err, res, body) {
+			var data, error;
+			try {
+				data = JSON.parse(body);
+			} catch (error) {
+				console.log(error)
+				return;
+			}
+			if(!data){
+				console.log(data);
+				msg.channel.sendMessage("Error:\n" + JSON.stringify(data));
+				return;
+			}
+			else if (!data.items || data.items.length == 0){
+				console.log(data);
+				msg.channel.sendMessage("No result for '" + args + "'");
+				return;
+			}
+			var result = data.items[0];
+			msg.channel.sendMessage(result.title + '\n' + result.link);
+		});
+	}
+}
+
+exports.google = {
+	usage: "<search query>",
+	description: "gets the top matching result from google",
+	process: function(bot, msg, args) {
+		if(!AuthDetails || !AuthDetails.youtube_api_key || !AuthDetails.google_custom_search){
+			msg.channel.sendMessage("Image search requires both a YouTube API key and a Google Custom Search key!");
+			return;
+		}
+
+		var imageQueryCleaned = (args.replace(/\s/g, '+'));
+		var searchURL = "https://www.googleapis.com/customsearch/v1?key=" + AuthDetails.youtube_api_key
+				+ "&cx=" + AuthDetails.google_custom_search
+				+ "&safe=medium&alt=json&q=" + imageQueryCleaned;
+
+		request(searchURL, function(err, res, body) {
+			var data, error;
+			try {
+				data = JSON.parse(body);
+			} catch (error) {
+				console.log(error)
+				return;
+			}
+			if(!data){
+				console.log(data);
+				msg.channel.sendMessage("Error:\n" + JSON.stringify(data));
+				return;
+			}
+			else if (!data.items || data.items.length == 0){
+				console.log(data);
+				msg.channel.sendMessage("No result for '" + args + "'");
+				return;
+			}
+			var result = data.items[0];
+			msg.channel.sendMessage(result.title + '\n' + result.link);
+		});
 	}
 }
